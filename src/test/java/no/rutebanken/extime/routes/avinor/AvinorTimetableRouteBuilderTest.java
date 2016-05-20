f249bacc019bbf1c27a9b418673d6baff7cc89cf
Samuel Wirzen <samuel.wirzen@webstep.no>
2016-05-20 14:31:25 +0200

    NRP-158 - Added test for TimetableFetcher route and associated aggregate

--- src/test/java/no/rutebanken/extime/routes/avinor/AvinorTimetableRouteBuilderTest.java
+++ src/test/java/no/rutebanken/extime/routes/avinor/AvinorTimetableRouteBuilderTest.java
@@ -1,5 +1,8 @@
 package no.rutebanken.extime.routes.avinor;
 
+import no.avinor.flydata.xjc.model.feed.Flight;
+import no.rutebanken.extime.model.FlightDirection;
+import no.rutebanken.extime.model.FlightType;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
@@ -10,16 +13,69 @@ import org.apache.camel.component.properties.DefaultPropertiesParser;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.model.ProcessorDefinition;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.ConnectException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.function.Predicate;
 
-import static no.rutebanken.extime.routes.avinor.AvinorTimetableRouteBuilder.HEADER_AIRPORT_IATA;
+import static no.rutebanken.extime.routes.avinor.AvinorTimetableRouteBuilder.*;
 
 public class AvinorTimetableRouteBuilderTest extends CamelTestSupport {
+
+    @Test
+    public void testAirportFlightsFeedRoute() throws Exception {
+        context.getRouteDefinition("TimetableFetcher").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                interceptSendToEndpoint("mock:flightFeedEndpoint")
+                        .process(exchange -> {
+                            InputStream inputStream = new FileInputStream("target/classes/xml/departures-osl.xml");
+                            exchange.getIn().setBody(inputStream);
+                        });
+                weaveAddLast().to("mock:output");
+            }
+        });
+        context.start();
+
+        getMockEndpoint("mock:flightFeedEndpoint").expectedMessageCount(1);
+        getMockEndpoint("mock:flightFeedEndpoint").expectedHeaderReceived(Exchange.HTTP_METHOD, HttpMethods.GET);
+        getMockEndpoint("mock:flightFeedEndpoint").expectedHeaderReceived(
+                Exchange.HTTP_QUERY, "airport=OSL&timeFrom=0&timeTo=72&direction=D");
+
+        getMockEndpoint("mock:error").expectedMessageCount(0);
+        getMockEndpoint("mock:output").expectedMessageCount(1);
+
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(HEADER_AIRPORT_IATA, "OSL");
+        headers.put(HEADER_FLIGHTS_TIMEFROM, "0");
+        headers.put(HEADER_FLIGHTS_TIMETO, "72");
+        headers.put(HEADER_FLIGHTS_DIRECTION, FlightDirection.DEPARTURE.getCode());
+
+        template.sendBodyAndHeaders("direct:fetchAirportFlights", "OSL", headers);
+
+        assertMockEndpointsSatisfied();
+
+        MockEndpoint mockOutput = getMockEndpoint("mock:output");
+        mockOutput.expectedMessageCount(1);
+        Object body = mockOutput.getReceivedExchanges().get(0).getIn().getBody();
+
+        Assertions.assertThat(body).asList()
+                .isNotNull()
+                .isInstanceOf(List.class);
+
+        Assertions.assertThat((List<Flight>) body)
+                .isNotEmpty()
+                .hasSize(3)
+                .doesNotHaveDuplicates()
+                .allMatch((Predicate<Flight>) flight -> flight.getDomInt().equalsIgnoreCase(FlightType.DOMESTIC.getCode()));
+    }
 
     @Test
     public void testAirportNameEnricherRoute() throws Exception {
@@ -54,14 +110,12 @@ public class AvinorTimetableRouteBuilderTest extends CamelTestSupport {
         getMockEndpoint("mock:airportFeedEndpoint").whenAnyExchangeReceived(
                 exchange -> exchange.setException(new ConnectException("SIMULATED: Connection timed out")));
 
-        MockEndpoint mockError = getMockEndpoint("mock:error");
-        mockError.expectedMessageCount(1);
-        mockError.message(0).exchangeProperty(Exchange.EXCEPTION_CAUGHT).isNotNull();
-
         getMockEndpoint("mock:output").expectedMessageCount(1);
 
         template.sendBodyAndHeader("direct:fetchAirportNameResource", "OSL", HEADER_AIRPORT_IATA, "OSL");
+
         assertMockEndpointsSatisfied();
+
     }
 
     @Override
