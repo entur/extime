cfcd5f7a99666186b5ac2c1616eba1e3f2248147
Samuel Wirzen <samuel.wirzen@webstep.no>
2016-05-20 11:59:37 +0200

    NRP-158 - Added unit test skeleton for AvinorTimetableRouteBuilder

---
+++ src/test/java/no/rutebanken/extime/routes/avinor/AvinorTimetableRouteBuilderTest.java
@@ -0,0 +1,58 @@
+package no.rutebanken.extime.routes.avinor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.properties.DefaultPropertiesParser;
+import org.apache.camel.component.properties.PropertiesComponent;
+import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class AvinorTimetableRouteBuilderTest extends CamelTestSupport {
+
+    @Test
+    public void testSample() throws Exception {
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        PropertiesComponent pc = (PropertiesComponent) context.getComponent("properties");
+        pc.setPropertiesParser(new DefaultPropertiesParser() {
+            @Override
+            public String parseProperty(String key, String value, Properties properties) {
+                Map<String, String> testProperties = new HashMap<String, String>() {{
+                    put("avinor.timetable.scheduler.cron", "direct:start");
+                }};
+                return testProperties.get(key);
+            }
+        });
+        return new AvinorTimetableRouteBuilder();
+    }
+
+    @Override
+    public boolean isUseAdviceWith() {
+        return true;
+    }
+
+    @Override
+    public boolean isUseDebugger() {
+        return true;
+    }
+
+    @Override
+    protected void debugBefore(Exchange exchange, Processor processor,
+                               ProcessorDefinition<?> definition, String id, String shortName) {
+        log.info("Before " + definition + " with body " + exchange.getIn().getBody());
+    }
+
+    @Override
+    protected void debugAfter(Exchange exchange, Processor processor,
+                              ProcessorDefinition<?> definition, String id, String label, long timeTaken) {
+        log.info("After " + definition + " with body " + exchange.getIn().getBody());
+    }
+}
