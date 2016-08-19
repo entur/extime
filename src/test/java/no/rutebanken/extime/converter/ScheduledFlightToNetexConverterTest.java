3dcc41e58c0911eaeea9c60209f07d694505ab18
Samuel Wirzen <samuel.wirzen@webstep.no>
2016-08-19 09:45:59 +0200

    NRP-547 - Added some tests for netex generation/conversion

--- src/test/java/no/rutebanken/extime/converter/ScheduledFlightToNetexConverterTest.java
+++ src/test/java/no/rutebanken/extime/converter/ScheduledFlightToNetexConverterTest.java
@@ -3,6 +3,11 @@ package no.rutebanken.extime.converter;
 import no.rutebanken.extime.AppTest;
 import no.rutebanken.extime.config.*;
 import no.rutebanken.extime.model.ScheduledDirectFlight;
+import no.rutebanken.netex.model.ContactStructure;
+import no.rutebanken.netex.model.Direction;
+import no.rutebanken.netex.model.MultilingualString;
+import no.rutebanken.netex.model.OrganisationTypeEnumeration;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -11,11 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.xml.bind.JAXBElement;
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {AppTest.class})
@@ -58,12 +63,98 @@ public class ScheduledFlightToNetexConverterTest {
     }
 
     @Test
-    @Ignore
-    public void convertDirectFlightToNetex() {}
+    public void testCreateDirection() {
+        Direction direction = clazzUnderTest.createDirection("WF739");
+
+        Assertions.assertThat(direction)
+                .isNotNull();
+        Assertions.assertThat(direction.getId())
+                .isNotNull()
+                .isNotEmpty()
+                .isEqualTo("AVI:Route:WF739101:Direction");
+    }
 
     @Test
-    @Ignore
-    public void convertStopoverFlightToNetex() {}
+    public void testCreateMultilingualString() {
+        MultilingualString multilingualString = clazzUnderTest.createMultilingualString("TEST-STRING");
+
+        Assertions.assertThat(multilingualString)
+                .isNotNull();
+        Assertions.assertThat(multilingualString.getValue())
+                .isNotNull()
+                .isNotEmpty()
+                .isEqualTo("TEST-STRING");
+    }
+
+    @Test
+    public void testCreateOrganisationRest() {
+        String companyNumber = "999999999";
+        String name = "TEST-COMPANY";
+        String legalName = "TEST-LEGAL-NAME";
+        String phone = "0047 999 99 999";
+        String url = "http://test.no";
+        OrganisationTypeEnumeration organisationType = OrganisationTypeEnumeration.OPERATOR;
+
+        List<JAXBElement<?>> organisationRest = clazzUnderTest.createOrganisationRest(
+                companyNumber, name, legalName, phone, url, organisationType
+        );
+
+        Assertions.assertThat(organisationRest)
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(5);
+        Assertions.assertThat(organisationRest.get(0).getValue())
+                .isNotNull()
+                .isInstanceOf(String.class)
+                .isEqualTo(companyNumber);
+
+        Assertions.assertThat(organisationRest.get(1).getValue())
+                .isNotNull()
+                .isInstanceOf(MultilingualString.class);
+        Assertions.assertThat(((MultilingualString)organisationRest.get(1).getValue()).getValue())
+                .isNotNull()
+                .isNotEmpty()
+                .isEqualTo(name);
+
+        Assertions.assertThat(organisationRest.get(2).getValue())
+                .isNotNull()
+                .isInstanceOf(MultilingualString.class);
+        Assertions.assertThat(((MultilingualString)organisationRest.get(2).getValue()).getValue())
+                .isNotNull()
+                .isNotEmpty()
+                .isEqualTo(legalName);
+
+        Assertions.assertThat(organisationRest.get(3).getValue())
+                .isNotNull()
+                .isInstanceOf(ContactStructure.class);
+
+        Assertions.assertThat(organisationRest.get(4).getValue())
+                .isNotNull()
+                .isInstanceOf(List.class);
+        Assertions.assertThat(((List<OrganisationTypeEnumeration>)organisationRest.get(4).getValue()).get(0))
+                .isNotNull()
+                .isEqualTo(organisationType);
+    }
+
+    @Test
+    public void testCreateContactStructure() {
+        String phone = "0047 999 99 999";
+        String url = "http://test.no";
+
+        JAXBElement<ContactStructure> contactStructureElement = clazzUnderTest.createContactStructure(phone, url);
+
+        Assertions.assertThat(contactStructureElement.getValue())
+                .isNotNull()
+                .isInstanceOf(ContactStructure.class);
+        Assertions.assertThat(contactStructureElement.getValue().getPhone())
+                .isNotNull()
+                .isNotEmpty()
+                .isEqualTo(phone);
+        Assertions.assertThat(contactStructureElement.getValue().getUrl())
+                .isNotNull()
+                .isNotEmpty()
+                .isEqualTo(url);
+    }
 
     public ScheduledDirectFlight createScheduledDirectFlight(String airlineIATA, String flightId) {
         ScheduledDirectFlight directFlight = new ScheduledDirectFlight();
