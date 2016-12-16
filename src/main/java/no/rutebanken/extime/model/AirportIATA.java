657e5b577ca56326e3a04c074da2413c4406ea9a
Samuel Wirzen <samuel.wirzen@webstep.no>
2016-12-16 14:06:10 +0100

    NRP-969 - Fixed bug by replacing non-avinor iata with missing iata VRY

--- src/main/java/no/rutebanken/extime/model/AirportIATA.java
+++ src/main/java/no/rutebanken/extime/model/AirportIATA.java
@@ -1,50 +1,51 @@
 package no.rutebanken.extime.model;
 
 public enum AirportIATA {
-    AES,
-    ALF,
-    ANX,
-    BDU,
-    BGO,
-    BJF,
-    BNN,
-    BOO,
-    BVG,
-    EVE,
-    FDE,
-    FRO,
-    HAA,
-    HAU,
-    HFT,
-    HOV,
-    HVG,
-    KKN,
-    KRS,
-    KSU,
-    LKL,
-    LKN,
-    LYR,
-    MEH,
-    MJF,
-    MOL,
-    MQN,
-    NVK,
-    OLA,
-    OSL,
-    OSY,
-    RET,
-    RRS,
-    RVK,
-    SDN,
-    SKN,
-    SOG,
-    SOJ,
-    SSJ,
-    SVG,
-    SVJ,
-    TOS,
-    TRD,
-    VAW,
-    VDB,
-    VDS
+
+    AES, // Ålesund lufthavn
+    ALF, // Alta lufthavn
+    ANX, // Andøya lufthavn
+    BDU, // Bardufoss lufthavn
+    BGO, // Bergen lufthavn
+    BJF, // Båtsfjord lufthavn
+    BNN, // Brønnøysund lufthavn
+    BOO, // Bodø lufthavn
+    BVG, // Berlevåg lufthavn
+    EVE, // Harstad/Narvik lufthavn
+    FDE, // Førde lufthamn
+    FRO, // Florø lufthamn
+    HAA, // Hasvik lufthavn
+    HAU, // Haugesund lufthavn
+    HFT, // Hammerfest lufthavn
+    HOV, // Ørsta-Volda lufthamn
+    HVG, // Honningsvåg lufthavn
+    KKN, // Kirkenes lufthavn
+    KRS, // Kristiansand lufthavn
+    KSU, // Kristiansund lufthavn
+    LKL, // Lakselv lufthavn
+    LKN, // Leknes lufthavn
+    LYR, // Svalbard lufthavn
+    MEH, // Mehamn lufthavn
+    MJF, // Mosjøen lufthavn
+    MOL, // Molde lufthavn
+    MQN, // Mo i Rana lufthavn
+    NVK, // Narvik lufthavn
+    OSL, // Oslo lufthavn
+    OSY, // Namsos lufthavn
+    RET, // Røst lufthavn
+    RRS, // Røros lufthavn
+    RVK, // Rørvik lufthavn
+    SDN, // Sandane lufthamn
+    SKN, // Stokmarknes lufthavn
+    SOG, // Sogndal lufthamn
+    SOJ, // Sørkjosen lufthavn
+    SSJ, // Sandnessjøen lufthavn
+    SVG, // Stavanger lufthavn
+    SVJ, // Svolvær lufthavn
+    TOS, // Tromsø lufthavn
+    TRD, // Trondheim lufthavn
+    VAW, // Vardø lufthavn
+    VDB, // Fagernes lufthamn
+    VDS, // Vadsø lufthavn
+    VRY  // Værøy helikopterhavn
 }
