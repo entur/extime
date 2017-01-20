76a15e9b99aeb7b5f519edae48afe60038a7d848
Samuel Wirzen <samuel.wirzen@webstep.no>
2017-01-20 10:46:38 +0100

    NRP-1016 - More complete test for full netex publication delivery export

--- src/test/java/no/rutebanken/extime/converter/LineDataToNetexConverterTest.java
+++ src/test/java/no/rutebanken/extime/converter/LineDataToNetexConverterTest.java
@@ -57,17 +57,25 @@ public class LineDataToNetexConverterTest {
 
         Map<String, Map<String, List<ScheduledFlight>>> routeJourneys = new HashMap<>();
 
+        LocalDate flight1DateOfOperation = LocalDate.parse("2017-01-30");
+        OffsetTime flight1DepartureTime = OffsetTime.parse("07:10:00Z");
+        OffsetTime flight1ArrivalTime = OffsetTime.parse("08:05:00Z");
+
         List<ScheduledFlight> routeOslBgoFlights = Lists.newArrayList(
-                createScheduledDirectFlight("DY", "DY602", LocalDate.parse("2017-01-30"), "OSL", "Oslo",
-                        "BGO", "Bergen", OffsetTime.parse("07:10:00Z"), OffsetTime.parse("08:05:00Z")));
+                createScheduledDirectFlight("DY", "DY602", flight1DateOfOperation, "OSL", "Oslo",
+                        "BGO", "Bergen", flight1DepartureTime, flight1ArrivalTime));
 
         Map<String, List<ScheduledFlight>> dy602Flights = new HashMap<>();
         dy602Flights.put("DY602", routeOslBgoFlights);
         routeJourneys.put("OSL-BGO", dy602Flights);
 
+        LocalDate flight2DateOfOperation = LocalDate.parse("2017-01-31");
+        OffsetTime flight2DepartureTime = OffsetTime.parse("17:00:00Z");
+        OffsetTime flight2ArrivalTime = OffsetTime.parse("17:55:00Z");
+
         List<ScheduledFlight> routeBgoOslFlights = Lists.newArrayList(
-                createScheduledDirectFlight("DY", "DY633", LocalDate.parse("2017-01-31"), "BGO", "Bergen",
-                        "OSL", "Oslo", OffsetTime.parse("17:00:00Z"), OffsetTime.parse("17:55:00Z")));
+                createScheduledDirectFlight("DY", "DY633", flight2DateOfOperation, "BGO", "Bergen",
+                        "OSL", "Oslo", flight2DepartureTime, flight2ArrivalTime));
 
         Map<String, List<ScheduledFlight>> dy633Flights = new HashMap<>();
         dy633Flights.put("DY633", routeBgoOslFlights);
@@ -96,6 +104,8 @@ public class LineDataToNetexConverterTest {
                 .isEqualTo(requestPeriodToDateTime);
 
         List<JAXBElement<? extends Common_VersionFrameStructure>> frames = compositeFrame.getFrames().getCommonFrame();
+
+        // check elements in service frame
         ServiceFrame serviceFrame = getFrames(ServiceFrame.class, frames).get(0);
 
         // check network
@@ -111,8 +121,6 @@ public class LineDataToNetexConverterTest {
         Assertions.assertThat(line.getOperatorRef().getRef()).isEqualTo("AVI:Operator:DY");
 
         Assertions.assertThat(line.getRoutes().getRouteRef())
-                .isNotNull()
-                .isNotEmpty()
                 .hasSize(2)
                 .extracting("ref")
                 .contains("AVI:Route:OSL-BGO", "AVI:Route:BGO-OSL");
@@ -126,11 +134,83 @@ public class LineDataToNetexConverterTest {
                 .collect(Collectors.toList());
 
         Assertions.assertThat(routes)
-                .isNotNull()
-                .isNotEmpty()
                 .hasSize(2)
                 .extracting("id", "name.value", "lineRef.value.ref")
                 .contains(tuple("AVI:Route:OSL-BGO", "Oslo-Bergen", "AVI:Line:DY-OSL-BGO"), tuple("AVI:Route:BGO-OSL", "Bergen-Oslo", "AVI:Line:DY-OSL-BGO"));
+
+        // check journey patterns
+        List<JAXBElement<?>> journeyPatternElements = serviceFrame.getJourneyPatterns().getJourneyPattern_OrJourneyPatternView();
+
+        List<JourneyPattern> journeyPatterns = journeyPatternElements.stream()
+                .map(JAXBElement::getValue)
+                .map(journeyPattern -> (JourneyPattern) journeyPattern)
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(journeyPatterns)
+                .hasSize(2)
+                .extracting("routeRef.ref")
+                .contains("AVI:Route:OSL-BGO", "AVI:Route:BGO-OSL");
+
+        journeyPatterns.forEach(journeyPattern -> Assertions.assertThat(journeyPattern.getPointsInSequence()
+                .getPointInJourneyPatternOrStopPointInJourneyPatternOrTimingPointInJourneyPattern()).hasSize(2));
+
+        // check elements in timetable frame
+        TimetableFrame timetableFrame = getFrames(TimetableFrame.class, frames).get(0);
+
+        List<ServiceJourney> serviceJourneys = timetableFrame.getVehicleJourneys().getDatedServiceJourneyOrDeadRunOrServiceJourney().stream()
+                .map(journey -> (ServiceJourney) journey)
+                .collect(Collectors.toList());
+
+        // check first service journey
+        Assertions.assertThat(serviceJourneys.get(0).getDepartureTime()).isEqualTo(flight1DepartureTime);
+        //Assertions.assertThat(serviceJourneys.get(0).getJourneyPatternRef().getValue().getRef()).isEqualTo("AVI:JourneyPattern:OSL-BGO"); // TODO fix journey pattern id
+        Assertions.assertThat(serviceJourneys.get(0).getPublicCode()).isEqualTo("DY602");
+        Assertions.assertThat(serviceJourneys.get(0).getLineRef().getValue().getRef()).isEqualTo("AVI:Line:DY-OSL-BGO");
+        Assertions.assertThat(serviceJourneys.get(0).getDayTypes().getDayTypeRef().get(0).getValue().getRef()).isEqualTo("AVI:DayType:Mon_30");
+
+        List<TimetabledPassingTime> departurePassingTimes = serviceJourneys.get(0).getPassingTimes().getTimetabledPassingTime();
+        Assertions.assertThat(departurePassingTimes).hasSize(2);
+        //Assertions.assertThat(departurePassingTimes.get(0).getPointInJourneyPatternRef().getValue().getRef()).isEqualTo(""); // TODO fix journey pattern id
+        Assertions.assertThat(departurePassingTimes.get(0).getDepartureTime()).isEqualTo(flight1DepartureTime);
+        //Assertions.assertThat(departurePassingTimes.get(1).getPointInJourneyPatternRef().getValue().getRef()).isEqualTo(""); // TODO fix journey pattern id
+        Assertions.assertThat(departurePassingTimes.get(1).getArrivalTime()).isEqualTo(flight1ArrivalTime);
+
+        // check second service journey
+        Assertions.assertThat(serviceJourneys.get(1).getDepartureTime()).isEqualTo(flight2DepartureTime);
+        //Assertions.assertThat(serviceJourneys.get(1).getJourneyPatternRef().getValue().getRef()).isEqualTo("AVI:JourneyPattern:OSL-BGO"); // TODO fix journey pattern id
+        Assertions.assertThat(serviceJourneys.get(1).getPublicCode()).isEqualTo("DY633");
+        Assertions.assertThat(serviceJourneys.get(1).getLineRef().getValue().getRef()).isEqualTo("AVI:Line:DY-OSL-BGO");
+        Assertions.assertThat(serviceJourneys.get(1).getDayTypes().getDayTypeRef().get(0).getValue().getRef()).isEqualTo("AVI:DayType:Tue_31");
+
+        List<TimetabledPassingTime> arrivalPassingTimes = serviceJourneys.get(1).getPassingTimes().getTimetabledPassingTime();
+        Assertions.assertThat(arrivalPassingTimes).hasSize(2);
+        //Assertions.assertThat(departurePassingTimes.get(0).getPointInJourneyPatternRef().getValue().getRef()).isEqualTo(""); // TODO fix journey pattern id
+        Assertions.assertThat(arrivalPassingTimes.get(0).getDepartureTime()).isEqualTo(flight2DepartureTime);
+        //Assertions.assertThat(departurePassingTimes.get(1).getPointInJourneyPatternRef().getValue().getRef()).isEqualTo(""); // TODO fix journey pattern id
+        Assertions.assertThat(arrivalPassingTimes.get(1).getArrivalTime()).isEqualTo(flight2ArrivalTime);
+
+        // check elements in service calendar frame
+        ServiceCalendarFrame serviceCalendarFrame = getFrames(ServiceCalendarFrame.class, frames).get(0);
+
+        // check day types
+        List<DayType> dayTypes = serviceCalendarFrame.getDayTypes().getDayType_().stream()
+                .map(JAXBElement::getValue)
+                .map(dayType -> (DayType) dayType)
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(dayTypes)
+                .hasSize(2)
+                .extracting("id")
+                .contains("AVI:DayType:Mon_30", "AVI:DayType:Tue_31");
+
+        LocalDate date = serviceCalendarFrame.getDayTypeAssignments().getDayTypeAssignment().get(0).getDate();
+        String ref = serviceCalendarFrame.getDayTypeAssignments().getDayTypeAssignment().get(0).getDayTypeRef().getValue().getRef();
+
+        // check day type assignments
+        Assertions.assertThat(serviceCalendarFrame.getDayTypeAssignments().getDayTypeAssignment())
+                .hasSize(2)
+                .extracting("date", "dayTypeRef.value.ref")
+                .contains(tuple(flight1DateOfOperation, "AVI:DayType:Mon_30"), tuple(flight2DateOfOperation, "AVI:DayType:Tue_31"));
     }
 
     private ScheduledFlight createScheduledDirectFlight(String airlineIATA, String airlineFlightId,
