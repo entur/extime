fbd8808aef6fbf7377d84ab97c1e391b057cd715
erlendnils1 <erlendnils1@gmail.com>
2018-01-29 11:07:18 +0100

    ROR-159 Demoted log statement to debug

--- src/main/java/no/rutebanken/extime/converter/ScheduledFlightConverter.java
+++ src/main/java/no/rutebanken/extime/converter/ScheduledFlightConverter.java
@@ -495,7 +495,7 @@ public class ScheduledFlightConverter {
         List<Flight> flightLegsForIata = flightsByArrivalAirportIata.get(departureAirportIata);
         Predicate<Flight> flightPredicate = FlightPredicate.matchPreviousFlight(currentFlightLeg);
 
-        logger.info("Attempting to find previous flight legs for flight leg: "+ currentFlightLeg);
+        logger.debug("Attempting to find previous flight legs for flight leg: "+ currentFlightLeg);
 
         Optional<Flight> optionalFlightLeg = findOptionalConnectingFlightLeg(flightPredicate, flightLegsForIata);
         optionalFlightLeg.ifPresent(flight -> {
