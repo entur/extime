70804837f21829c930fdf3c2e3fe0a07115145b8
erlendnils1 <erlendnils1@gmail.com>
2018-10-15 15:08:05 +0200

    Added Air Leap as new airline

--- src/main/java/no/rutebanken/extime/model/AirlineDesignator.java
+++ src/main/java/no/rutebanken/extime/model/AirlineDesignator.java
@@ -4,8 +4,8 @@ import java.util.EnumSet;
 
 public enum AirlineDesignator {
 
-    DY, SK, WF, M3, LTR, VF;
+    DY, SK, WF, M3, LTR, VF, DX;
 
-    public static EnumSet<AirlineDesignator> commonDesignators = EnumSet.of(DY, SK, WF, M3, LTR, VF);
+    public static EnumSet<AirlineDesignator> commonDesignators = EnumSet.of(DY, SK, WF, M3, LTR, VF, DX);
 
 }
