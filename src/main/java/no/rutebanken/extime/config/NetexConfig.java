96d1a89674baef853140ed8b1010ff10d01bd7f4
erlendnils1 <erlendnils1@gmail.com>
2018-12-13 12:29:38 +0100

    Updated dependencies. spring boot + camel + logstash + activemq + guava + ehcache ++

--- src/main/java/no/rutebanken/extime/config/NetexConfig.java
+++ src/main/java/no/rutebanken/extime/config/NetexConfig.java
@@ -1,6 +1,5 @@
 package no.rutebanken.extime.config;
 
-import no.rutebanken.extime.util.NetexObjectFactory;
 import org.rutebanken.netex.model.ObjectFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +10,5 @@ public class NetexConfig {
     @Bean
     public ObjectFactory objectFactory() {return new ObjectFactory();}
 
-    @Bean
-    public NetexObjectFactory netexObjectFactory() {return new NetexObjectFactory();}
 
 }
