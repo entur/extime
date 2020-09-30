e764e7e1209112fd79fc915f5232d012bd0c992d
vincent <vincent.paturet@entur.org>
2020-09-30 10:05:51 +0200

    WTK-83 Marduk buckets migrations - update IAM-roles

--- terraform/environments/radon.tfvars
+++
@@ -1,7 +0,0 @@
-gcp_project = "radon-1287"
-gcp_pubsub_project = "entur-ror-test"
-gcp_legacy_project = "radon-1287"
-kube_namespace = "staging"
-bucket_instance_name = "marduk-exchange-stage"
-location = "europe-west1"
-service_account_bucket_role = "roles/storage.admin"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,9 +11,6 @@ variable "gcp_legacy_project" {
   description = "The legacy GCP project "
 }
 
-variable "location" {
-  description = "GCP bucket location"
-}
 variable "kube_namespace" {
   description = "The Kubernetes namespace"
 }
@@ -33,45 +30,14 @@ variable "bucket_instance_name" {
   description = "Storage Bucket name"
 }
 
-variable "force_destroy" {
-  description = "(Optional, Default: false) When deleting a bucket, this boolean option will delete all contained objects. If you try to delete a bucket that contains objects, Terraform will fail that run"
-  default = false
-}
-
-variable "storage_class" {
-  description = "GCP storage class"
-  default = "REGIONAL"
-}
-
-variable "versioning" {
-  description = "The bucket's Versioning configuration."
-  default = "true"
-}
-
-variable "log_bucket" {
-  description = "The bucket's Access & Storage Logs configuration"
-  default = "false"
-}
-
-variable "bucket_policy_only" {
-  description = "Enables Bucket Policy Only access to a bucket"
-  default = "false"
-}
-
 variable "service_account_bucket_role" {
   description = "Role of the Service Account - more about roles https://cloud.google.com/storage/docs/access-control/iam-roles"
-  default = "roles/storage.objectViewer"
+  default = "roles/storage.objectCreator"
 }
 
 variable "service_account_pubsub_role" {
   description = "Role of the Service Account - more about roles https://cloud.google.com/pubsub/docs/access-control"
-  default = "roles/pubsub.editor"
-}
-
-variable "prevent_destroy" {
-  description = "Prevent destruction of bucket"
-  type = bool
-  default = false
+  default = "roles/pubsub.publisher"
 }
 
 variable "load_config_file" {
