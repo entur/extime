494f0f9046971800f92af0aea058c50612e46cfe
vincent <vincent.paturet@entur.org>
2020-10-30 10:09:33 +0100

    Added Air Leap airline

--- src/main/java/no/rutebanken/extime/model/AirlineDesignator.java
+++ src/main/java/no/rutebanken/extime/model/AirlineDesignator.java
@@ -6,9 +6,9 @@ import java.util.Set;
 public enum AirlineDesignator {
 
     DY, SK, WF, M3, LTR, VF, DX, FI, AY, BT, D8, RC, FR, EJU, KL, W2,
-    AF, BA, LH, LM, LO, SN, SU, W6, DK;
+    AF, BA, LH, LM, LO, SN, SU, W6, DK, FL;
 
     public static Set<AirlineDesignator> commonDesignators = EnumSet.of(DY, SK, WF, M3, LTR, VF, DX, FI, AY, BT, D8,
-            RC, FR, EJU, KL, W2, AF, BA, LH, LM, LO, SN, SU, W6, DK);
+            RC, FR, EJU, KL, W2, AF, BA, LH, LM, LO, SN, SU, W6, DK, FL);
 
 }
