d0f3aa6be19e6187627015b856b569fc9d44a616
vpaturet <46598384+vpaturet@users.noreply.github.com>
2022-01-12 10:57:53 +0100

    Remove obsolete airlines (#85)

--- src/main/java/no/rutebanken/extime/model/AirlineDesignator.java
+++ src/main/java/no/rutebanken/extime/model/AirlineDesignator.java
@@ -5,10 +5,10 @@ import java.util.Set;
 
 public enum AirlineDesignator {
 
-    DY, SK, WF, M3, LTR, VF, DX, FI, AY, BT, D8, RC, FR, EJU, KL, W2,
-    AF, BA, LH, LM, LO, SN, SU, W6, DK, FL, FS;
+    DY, SK, WF, M3, LTR, DX, FI, AY, BT, D8, RC, FR, EJU, KL, W2,
+    AF, BA, LH, LO, SN, SU, W6, FL, FS;
 
-    public static Set<AirlineDesignator> commonDesignators = EnumSet.of(DY, SK, WF, M3, LTR, VF, DX, FI, AY, BT, D8,
-            RC, FR, EJU, KL, W2, AF, BA, LH, LM, LO, SN, SU, W6, DK, FL, FS);
+    public static Set<AirlineDesignator> commonDesignators = EnumSet.of(DY, SK, WF, M3, LTR, DX, FI, AY, BT, D8,
+            RC, FR, EJU, KL, W2, AF, BA, LH, LO, SN, SU, W6, FL, FS);
 
 }
