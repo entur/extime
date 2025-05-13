ff9a9a0b9f7a0727c80eba867bdce560c65ff2c7
Vincent Paturet <vincent.paturet@entur.org>
2025-05-13 16:00:53 +0200

    Build with Maven Central

--- src/test/java/no/rutebanken/extime/ExtimeRouteBuilderIntegrationTestBase.java
+++ src/test/java/no/rutebanken/extime/ExtimeRouteBuilderIntegrationTestBase.java
@@ -1,19 +1,68 @@
 package no.rutebanken.extime;
 
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PubSubEmulatorContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 
-/**
- * Integration test base class that keeps CamelContext between tests. Useful when camel is not used expicitly in tests.
- */
 @CamelSpringBootTest
-@ActiveProfiles({"default", "in-memory-blobstore", "google-pubsub-emulator"})
+@ActiveProfiles({"default", "in-memory-blobstore", "google-pubsub-autocreate"})
 @SpringBootTest(classes = {App.class}, properties = {
         "avinor.timetable.scheduler.consumer=direct:start"
 })
+@Testcontainers
 public abstract class ExtimeRouteBuilderIntegrationTestBase {
 
+    private static PubSubEmulatorContainer pubsubEmulator;
+
+    @Autowired
+    protected CamelContext context;
+
+    @Autowired
+    protected PubSubTemplate pubSubTemplate;
+
+
+    @Produce("direct:start")
+    protected ProducerTemplate startTemplate;
+
+    @BeforeAll
+    public static void init() {
+        pubsubEmulator =
+                new PubSubEmulatorContainer(
+                        DockerImageName.parse(
+                                "gcr.io/google.com/cloudsdktool/cloud-sdk:emulators"
+                        )
+                );
+        pubsubEmulator.start();
+    }
+
+    @DynamicPropertySource
+    static void emulatorProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.cloud.gcp.pubsub.emulator-host",
+                pubsubEmulator::getEmulatorEndpoint
+        );
+        registry.add(
+                "camel.component.google-pubsub.endpoint",
+                pubsubEmulator::getEmulatorEndpoint
+        );
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        pubsubEmulator.stop();
+    }
 
 }
--- src/test/java/no/rutebanken/extime/routes/avinor/AvinorTimetableRouteBuilderPubSubIntegrationTest.java
+++ src/test/java/no/rutebanken/extime/routes/avinor/AvinorTimetableRouteBuilderPubSubIntegrationTest.java
@@ -1,14 +1,16 @@
 package no.rutebanken.extime.routes.avinor;
 
 import com.google.pubsub.v1.PubsubMessage;
-import no.rutebanken.extime.ExtimeCamelRouteBuilderIntegrationTestBase;
+import no.rutebanken.extime.ExtimeRouteBuilderIntegrationTestBase;
 import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.test.spring.junit5.UseAdviceWith;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 import java.util.Map;
@@ -28,7 +30,9 @@ import static no.rutebanken.extime.routes.avinor.AvinorTimetableRouteBuilder.HEA
         "netex.compressed.output.path=target/marduk-mock",
         "avinor.timetable.dump.output.path=target/flights"
 })
-class AvinorTimetableRouteBuilderPubSubIntegrationTest extends ExtimeCamelRouteBuilderIntegrationTestBase {
+@UseAdviceWith
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class AvinorTimetableRouteBuilderPubSubIntegrationTest extends ExtimeRouteBuilderIntegrationTestBase {
 
     @Value("${queue.upload.destination.name}")
     private String notificationQueue;
